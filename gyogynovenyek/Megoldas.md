1ee3a2fd9544709dde3de8572d2d45b3ffd1b76c
lajtosdavid11 <lajtos.david11@gmail.com>
2020-10-12 12:44:16 +0200

    Dogagyakorlas

--- gyogynovenyek/Megoldas.md
+++ gyogynovenyek/Megoldas.md
@@ -7,11 +7,46 @@ Kétféféle mód közül bármelyik ctrl+. vagy jobb klikk +new item + add
 - 1 paraméter string 
   - például: Acsalapu;levél;6;8 
   - ;-jellel vannak elválasztva
+  - átadott fájlból beolvasott sort:
+   - ';' Mentén tagoljuk
+   - [0] --> Nev
+   - [1] --> resz
+   - [2] --> kezd
+   - [3] --> Veg
+   - [4] --> idotartam
+   - idotartam --> számolása (következő pont)
   - 4 db adat 5 tulajdonság?
   - 5. tulajdonság az időtartam ami azt jelenti --> Időtartam
     -  Konstruktorban számoljuk
     -  Számolása vég - kezdet ha a vég nagyobb mint a kezdet
     -  Vég > kezdet akkor  --> vég-kezdet+1 
-    -  egyébként --> vég+kezdet+1;
-
+    -  egyébként --> 12 - vég + kezdet + 1;
+ # kezdés
+    - Adatok beolvasása és tárolása
+  - Listában tároljuk a példányokat
+   --- System.IO, streamreader, while
+   --- cikluson belül listához hozzáadunk egy új példányt (beolvasott sor a paraméter)
+ # 1.feladat
+ --- Hány növény van ez a lista count tulajdonság
+# 2.feladat
+ --- Milyen gyüjthető részek vannak?
+    --- megszámolása ezeknek?
+    --- adatszerkezet Dictionary<string, int>
+    --- 1.Milyenek a gyüjthető részek:
+    --- foreach "noveny"
+    --- Ha a rész nem kulcs --> akkor adunk hozzá 0-át
+    --- Hány db van ezekből
+    --- Foreach "noveny"
+    ---Ha a "Resz" == Key --> Values++;
+foreach
+# 3.feladat
+    --- maximum kereses --> időtartam alapján
+--- Növény(ek) amelyeket a legtöbb ideig szedtek
+# 4.feladat
+    --- átlag számítás időtartam alapján
+--- osszeg valtozo
+--- atlga valtozo
+--- for végig a noveny-en
+--- osszeg += idotartam
+--- atlag = szum/count
 
--- gyogynovenyek/Program.cs
+++ gyogynovenyek/Program.cs
@@ -3,14 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace gyogynovenyek
 {
     class Program
     {
-        
+        static List<novenyek> noveny = new List<novenyek>();
+        static Dictionary<string, int> reszek = new Dictionary<string, int>();
+
+
+        static void Beolv()
+        {
+            StreamReader sr = new StreamReader("noveny.csv");
+            while (!sr.EndOfStream)
+            {
+                noveny.Add(new novenyek(sr.ReadLine()));
+            }
+
+            sr.Close();
+        }
+
+        static void elso()
+        {
+            Console.WriteLine("Növények száma {0} db",noveny.Count()); 
+
+
+            //int osszeg = 0;
+            //for (int i = 0; i < noveny.Count; i++)
+            //{
+            //    osszeg++;
+            //}
+            //Console.WriteLine("Növények száma: {0}",osszeg);
+        }
+        static void masodik()
+
+        {
+            Console.WriteLine("Gyüjthető részek:");
+            foreach (var t in noveny)
+            {
+                if (!reszek.ContainsKey(t.Resz))
+                {
+                    reszek.Add(t.Resz, 0);
+                   
+                }
+                
+            }
+            foreach (var t in noveny)
+            {
+                reszek[t.Resz]++;
+            }
+            foreach (var m in reszek)
+            {
+                Console.WriteLine($"{m.Key} : {m.Value}");
+            }
+        }
+
+
+        static void harmas()
+        {
+            int max = 0;
+            foreach (var t in noveny)
+            {
+                if ( t.Idotartam > max)
+                {
+                    max = t.Idotartam;
+                }
+            }
+            Console.WriteLine("Legtöbb idő {0}", max);
+
+            foreach (var t in noveny)
+            {
+                if (t.Idotartam == max)
+                {
+                    Console.WriteLine(t.Nev);
+                }
+            }
+        }
+
+
+        static void negyes()
+        {
+            Console.WriteLine("\n");
+            double szum = 0;
+            
+            foreach (var t in noveny)
+            {
+                szum += t.Idotartam;
+                
+            }
+            Console.WriteLine("átlag: {0}",szum/noveny.Count());
+        }
         static void Main(string[] args)
         {
+            Beolv();
+            elso();
+            masodik();
+            harmas();
+            negyes();
+            //foreach (var t in noveny)
+            //{
+            //    Console.WriteLine(t.Nev +";" + t.Resz+ ";" + t.Kezd + ";"+ t.Veg) ;
+            //}
+
             Console.ReadKey();
         }
     }
